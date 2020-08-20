5a3a67f9249728c7afc55c922c4be9bcc688a79f
cristinapontiijas <cristinapontijas@gmail.com>
2020-08-20 22:27:42 +0200

    create PhoneItem stylesheet

--- src/components/PhoneItem.js
+++ src/components/PhoneItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import '../stylesheets/PhoneItem.scss';
 
 const PhoneItem = (props) => {
   // console.log(props.url);
   return (
-    <div>
+    <section className="card__container">
       <h2>{props.name}</h2>
       <p>{props.manufacturer}</p>
       <img src="https://via.placeholder.com/200.png/09f/fff" alt="phone"></img>
-    </div>
+    </section>
   );
 };
 
