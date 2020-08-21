2c4bd55bf2a3405b470db6f33c28359e6255b390
cristinapontiijas <cristinapontijas@gmail.com>
2020-08-21 17:06:02 +0200

    update modal styles

--- src/components/PhoneItemDetail.js
+++ src/components/PhoneItemDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../stylesheets/PhoneItemDetail.scss';
+import phone2 from '../images/Galaxy_S7.png';
 
 const PhoneItemDetail = (props) => {
   console.log(props.phones);
@@ -17,23 +18,23 @@ const PhoneItemDetail = (props) => {
             <button>Go Back</button>
           </div>
           <div className="modal__info">
-            <img src="" tile="" alt="phone" className="modal__img"></img>
+            <img src={phone2} tile="" alt="phone" className="modal__img"></img>
             <section className="modal__properties">
               <ul className="modal__list">
-                <li>
+                <li className="modal__listItem">
                   <p className="modal__description">Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, laborum officiis! Perferendis corporis perspiciatis suscipit molestiae! Molestiae cupiditate accusantium in delectus culpa et suscipit tempore quisquam? Earum quos aut iure.</p>
                 </li>
-                <li>
-                  <small>color:</small>lorem ipsum ...
+                <li className="modal__listItem">
+                  <small>Color: </small>lorem ipsum ...
                 </li>
-                <li>
-                  <small>screen:</small>lorem ipsum ...
+                <li className="modal__listItem">
+                  <small>Screen: </small>lorem ipsum ...
                 </li>
-                <li>
-                  <small>processor:</small>lorem ipsum ...
+                <li className="modal__listItem">
+                  <small>Processor: </small>lorem ipsum ...
                 </li>
-                <li>
-                  <small>ram:</small>lorem ipsum ...
+                <li className="modal__listItem">
+                  <small>Ram: </small>lorem ipsum ...
                 </li>
                 <li>price</li>
               </ul>
