5bc4e9dca5e7d15a9d6b27eb90476f8a1fcc089b
cristinapontiijas <cristinapontijas@gmail.com>
2020-08-21 12:44:29 +0200

    update PhoneItemDetail styles

--- src/components/PhoneItemDetail.js
+++ src/components/PhoneItemDetail.js
@@ -5,18 +5,18 @@ const PhoneItemDetail = (props) => {
   console.log('using PhoneItemDetail');
   return (
     <article className="modal__container">
-      <div>
+      <div className="modal__header">
         <section>
           <h2>Phone Name</h2>
           <p>Manufacturer</p>
         </section>
         <button>Go Back</button>
       </div>
-      <div>
-        <img src="" tile="" alt="phone"></img>
-        <section>
-          <p>Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, laborum officiis! Perferendis corporis perspiciatis suscipit molestiae! Molestiae cupiditate accusantium in delectus culpa et suscipit tempore quisquam? Earum quos aut iure.</p>
-          <ul>
+      <div className="modal__info">
+        <img src="" tile="" alt="phone" className="modal__img"></img>
+        <section className="modal__properties">
+          <p className="modal__description">Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, laborum officiis! Perferendis corporis perspiciatis suscipit molestiae! Molestiae cupiditate accusantium in delectus culpa et suscipit tempore quisquam? Earum quos aut iure.</p>
+          <ul className="modal__list">
             <li>
               <small>color:</small>lorem ipsum ...
             </li>
