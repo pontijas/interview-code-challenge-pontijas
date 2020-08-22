a4a59caf5b87e6898f668eab0bab7372def29c11
cristinapontiijas <cristinapontijas@gmail.com>
2020-08-22 15:46:59 +0200

    delete services files

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import './stylesheets/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const express = require('express');
-const app = express();
-app.listen(3000, () => {
-  console.log('El servidor está inicializado en el puerto 3000');
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
